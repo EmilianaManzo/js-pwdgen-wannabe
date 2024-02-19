84769a423a0d7a90623970dc9e564520320c0b7d
EmilianaManzo <emiliana.ma96@gmail.com>
2024-02-19 15:10:27 +0100

    [add] list item 1-2-3-4

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo co
 2. con un prompt chiedo all'utente il suo cognome e lo salvo in una variabile. 
 3. con un prompt chiedo all'utente il suo colore preferito e lo salvo in una variabile. 
 4. seleziono l'elemento dove inserire l'output e utilizzo innerHTML per modificare l'html
+
--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,17 @@
-const name = prompt('Come ti chiami?')
+//1
+const firstName = prompt('Scrivi qui il tuo nome');
+
+//2
+const lastName = prompt('Scrivi qui il tuo cognome');
+
+//3
+const favColor = prompt('Scrivi qui il tuo colore preferito');
+
+//4
+const password = firstName + lastName + favColor 
+
+//5
+document .getElementById('output')
+
+console.log(firstName, lastName, favColor, password);
+
